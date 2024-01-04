ba11db20a1ea5ca37796a564bbc8543632959b5a
AmuroBrian <perezbrian.sti@gmail.com>
2024-01-04 09:49:10 +0800

    Changes its Theme into light

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const menubar = document.getElementById('menubar');
 const menulist = document.querySelector('.menulist');
 const bar = document.querySelector('.menubar');
-const main = document.querySelectorAll('.main1');
-let navbar = document.querySelector('.navbar');
+const main = document.querySelectorAll('.show');
+let navbar = document.getElementById('navbar');
 
+
+// console.log(bar);
+// console.log(main);
+// console.log(menulist);
 menubar.addEventListener('click', ()=>{
     bar.classList.toggle('is-active');
     menulist.classList.toggle('active');
@@ -15,7 +19,7 @@ const observer = new IntersectionObserver(entries => {
         menulist.classList.toggle('is-black', entry.isIntersecting)
     })
 }, {
-    threshold: 0.5
+    threshold: 0.2
 }
 )
 
